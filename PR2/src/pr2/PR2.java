adf913dbefd0b9c5d592e8a9a641a1aae56d8467
JBarcenilla <JBarcenilla@ORD-COI-SIM-19>
2013-09-24 21:19:01 +0200

    otra version

---
+++ PR2/src/pr2/PR2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr2;
+
+/**
+ *
+ * @author JBarcenilla
+ */
+public class PR2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
